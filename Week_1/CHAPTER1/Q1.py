586591c9551ae2564b755561a4c99e24d29f11cb
Anu48 <anoushka212@gmail.com>
2021-05-28 16:24:16 -0400

    Testing

--- Week_1/CHAPTER1/Q1.py
+++ Week_1/CHAPTER1/Q1.py
@@ -2,3 +2,8 @@
 # Is Unique: Implement an algorithm to determine if a string has all unique characters. What if you
 # cannot use additional data structures?
 # Hints: #44, #7 7 7, #732
+# 1 + 1 = 2?
+def foo():
+    return 1 + 1 == 2
+
+print(foo())
