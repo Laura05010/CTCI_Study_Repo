6a5b2937aaf78379d65990193e497ce30a770621
aleeZArefeen <aleeza.arefeen@mail.utoronto.ca>
2021-05-29 13:22:49 -0400

    Added Hash function for Hashtable implementation

--- Week_1/RESOURCES/hashtable.py
+++ Week_1/RESOURCES/hashtable.py
@@ -12,6 +12,21 @@ class HashTable:
         # Bucket: Internal array, storing each inserted value in a “bucket” based on the provided key.
         self.buckets = [None] * self.capacity
 
+    def hash(self, key):
+        # iterator for each character in the key
+        hashsum = 0
+        # loop through keys for nodes in bucket
+        # enumerate() adds counter to the iterable, where the return object is an enumerate object
+        for index, char in enumerate(key):
+            #Add (index + key length)**(current char code)
+            # Hashsum is the overall representation of the key, we add the index to make the Unicode unique (uniformity is crucial in Hashtables)
+            hashsum += (index + len(key)) ** ord(char)
+            # The ord() function returns an integer representing the Unicode character, the reverse is char().
+            # char() converts an Integer to a Character, while ord() does the reverse.
+            # The modulus ensures the hashsum is in the range [0, self.capacity - 1].
+            hashsum = hashsum % self.capacity
+        return hashsum
+
 class Node:
     def __init__(self, key, value):
         # [(Laura, 10) -> (Max, 52) ->] [() ->] [() ->] [(Albert,100) ->(Anoushka, 48)->] [() ->] [(Aleeza,12) ->]
@@ -20,3 +35,4 @@ class Node:
         self.key = key
         self.value = value
         self.next = None # the next node
+
