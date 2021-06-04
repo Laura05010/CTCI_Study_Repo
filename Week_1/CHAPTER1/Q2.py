b976e6b7902773422d8802a264c840cb89ebfa23
Laura05010 <lcmmadrid5@gmail.com>
2021-06-04 15:05:40 -0400

    added a bit to question 2

--- Week_1/CHAPTER1/Q2.py
+++ Week_1/CHAPTER1/Q2.py
@@ -6,4 +6,21 @@
 # APPROACHES:
 # ---------------------------------------------------------------
 # LAURA:
-# -> Using a hashmap of the word and it’s permutations as a values then use the other word to see if its equal to any of the permutations.
+# -> Using a list to store the main permutations of the word to be checked then use the other word to see if its equal to any of the permutations.
+
+from typing import List
+
+def is_permutation(string1:str, string2:str) -> bool:
+    # Get the permutations of the string you want to check
+    permutations = get_permutations(string2)
+    # Check if any of these permutations matches the string
+    for permutation in permutations:
+        if string1 == permutation:
+            return True
+    # by this point there are no matching permutations, so:
+    return False
+
+
+# def get_permutations(string2:str) -> List:
+
+
